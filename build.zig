a4293644e3792525a0ee0179339d0d13c0e2051f
Francisco Nevitt Gonçalves <git@francisco.wiki>
2026-02-09 22:10:44 +0000

    Add benchmark and benchmark_main static libs

--- build.zig
+++ build.zig
@@ -1,5 +1,75 @@
 const std = @import("std");
+const zon = @import("build.zig.zon");
 
 pub fn build(b: *std.Build) void {
-    _ = b; // stub
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const benchmark_dep = b.dependency("benchmark", .{});
+
+    const benchmark = b.addLibrary(.{
+        .name = "benchmark",
+        .root_module = b.createModule(.{ .target = target, .optimize = optimize }),
+    });
+    benchmark.root_module.addCSourceFiles(.{
+        .root = benchmark_dep.path("src"),
+        .files = &.{
+            "benchmark.cc",
+            "benchmark_api_internal.cc",
+            "benchmark_name.cc",
+            "benchmark_register.cc",
+            "benchmark_runner.cc",
+            "check.cc",
+            "colorprint.cc",
+            "commandlineflags.cc",
+            "complexity.cc",
+            "console_reporter.cc",
+            "counter.cc",
+            "csv_reporter.cc",
+            "json_reporter.cc",
+            "perf_counters.cc",
+            "reporter.cc",
+            "statistics.cc",
+            "string_util.cc",
+            "sysinfo.cc",
+            "timers.cc",
+        },
+        .flags = &.{ "-std=c++17", "-fstrict-aliasing" },
+    });
+    switch (target.result.os.tag) {
+        .wasi => {},
+        .windows => benchmark.linkSystemLibrary("shlwapi"),
+        .linux => {
+            benchmark.linkSystemLibrary("pthread");
+            benchmark.linkSystemLibrary("rt");
+            benchmark.root_module.addCMacro("BENCHMARK_HAS_PTHREAD_AFFINITY", "1");
+        },
+        .solaris => benchmark.linkSystemLibrary("kstat"),
+        else => benchmark.linkSystemLibrary("pthread"),
+    }
+    if (target.result.os.tag != .windows) {
+        benchmark.root_module.addCMacro("_FILE_OFFSET_BITS", "64");
+        benchmark.root_module.addCMacro("_LARGEFILE64_SOURCE", "1");
+        benchmark.root_module.addCMacro("_LARGEFILE_SOURCE", "1");
+    }
+    benchmark.root_module.addCMacro("BENCHMARK_STATIC_DEFINE", "1");
+    benchmark.root_module.addCMacro("BENCHMARK_VERSION", "\"" ++ zon.version ++ "\"");
+    benchmark.root_module.addIncludePath(benchmark_dep.path("include"));
+    benchmark.installHeadersDirectory(benchmark_dep.path("include"), ".", .{});
+    benchmark.linkLibCpp();
+    b.installArtifact(benchmark);
+
+    const benchmark_main = b.addLibrary(.{
+        .name = "benchmark_main",
+        .root_module = b.createModule(.{ .target = target, .optimize = optimize }),
+    });
+    benchmark_main.root_module.addCSourceFiles(.{
+        .root = benchmark_dep.path("src"),
+        .files = &.{"benchmark_main.cc"},
+        .flags = &.{"-std=c++17"},
+    });
+    benchmark_main.root_module.addIncludePath(benchmark_dep.path("include"));
+    benchmark_main.installHeadersDirectory(benchmark_dep.path("include"), ".", .{});
+    benchmark_main.linkLibCpp();
+    b.installArtifact(benchmark_main);
 }
